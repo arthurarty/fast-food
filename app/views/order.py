bb4774706edf4cfb63f76f6273adfd14bd581076
ARTY-PC\Arty <arthurnangai@live.com>
2018-09-16 22:39:45 +0300

    add logic for endpoint.

--- app/views/order.py
+++ app/views/order.py
@@ -1,8 +1,11 @@
 from app.views import app
 from app.models.order import Order
+from app.models.restuarant import Restuarant
 from flask import Flask, jsonify, request
 
 empty_field = {'msg': 'A field is empty'}
+fast_food = Restuarant()
+
 
 @app.route('/v1/orders', methods=['POST'])
 def post_order():
@@ -14,8 +17,15 @@ def post_order():
 
     if request.json.get('customer_name'):
         new_order = Order('Nangai', 'Chapati', '10')
+        fast_food.add_order(new_order)
         return jsonify({"msg": "Order has been added"}), 201
 
     else:
         output = empty_field
         return jsonify(output), 400
+
+
+@app.route('/v1/orders', methods=['GET'])
+def get_orders():
+    res = fast_food.get_orders()
+    return jsonify(res)

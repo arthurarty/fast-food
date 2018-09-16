b74a176846e550488f0f68d3ad3e424930a24aba
ARTY-PC\Arty <arthurnangai@live.com>
2018-09-16 22:49:14 +0300

    change storage from dictionary to list.

--- app/models/restuarant.py
+++ app/models/restuarant.py
@@ -3,12 +3,11 @@ class Restuarant:
 
     def __init__(self):
         """initate an instance of class resturant"""
-        self.orders = {}
+        self.orders = []
 
     def add_order(self, order):
         """add a new order """
-        no_of_orders = len(self.orders) + 1
-        self.orders[no_of_orders] = order.__dict__
+        self.orders.append(order.__dict__)
         return self.orders
 
     def get_orders(self):

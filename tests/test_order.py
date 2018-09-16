31edfadca771f9a95be3376de8c8c3bf9afef573
ARTY-PC\Arty <arthurnangai@live.com>
2018-09-16 23:26:15 +0300

    improve tests.

--- tests/test_order.py
+++ tests/test_order.py
@@ -9,14 +9,17 @@ def test_post_order(client):
         "item_name": "Chapati",
         "quantity": "5", })
     assert resp.status_code == 201
+    assert b'Order has been added' in resp.data
 
 
 def test_get_orders(client):
     resp = client.get(
         '/v1/orders')
+    assert b'customer_name' in resp.data
     assert resp.status_code == 200
 
 
 def test_get_single_order(client):
-    resp = client.get('/v1/orders/1')
+    resp = client.get('/v1/orders/1', follow_redirects=True)
     assert resp.status_code == 200
+    assert b'customer_name' in resp.data

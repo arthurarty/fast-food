1334c09aba6cb39a7a0fbfd6ffb8895c3c4c90d5
ARTY-PC\Arty <arthurnangai@live.com>
2018-09-16 22:39:03 +0300

    add test for route

--- app/models/order.py
+++ app/models/order.py
@@ -1,5 +1,16 @@
 class Order:
+    """Class Order represents an order placed by a customer"""
+
     def __init__(self, customer_name, item_name, quantity):
+        """initiate an instance of class Order"""
         self.customer_name = customer_name
         self.item_name = item_name
         self.quantity = quantity
+
+    def get_customer_name(self):
+        """method returns customer name"""
+        return self.customer_name
+
+    def get_item_name(self):
+        """method returns item name"""
+        return self.item_name

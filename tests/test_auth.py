7b72d2037403a11371b8ff518d64389a5a547002
ARTY-PC\Arty <arthurnangai@live.com>
2018-09-29 21:22:42 +0300

    feat: add tests for signup and login.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -32,3 +32,54 @@ def test_user_login(client):
         "password": "testAs1v"})
     assert b'Successful login' in resp.data
     assert resp.status_code == 200
+
+
+def test_long_name(client):
+    resp = post_json(client, '/v1/auth/signup', {
+        "email": "test@test.com",
+        "name": "testismeyoutoova",
+        "password": "testsfas"})
+    assert b'Name is too long' in resp.data
+    assert resp.status_code == 400
+
+
+def test_invalid_name(client):
+    resp = post_json(client, '/v1/auth/signup', {
+        "email": "test@test.com",
+        "name": "testAsBA",
+        "password": "testsfas"})
+    assert b'Name can only contain lowercase a-z, 0-9 and _' in resp.data
+    assert resp.status_code == 400
+
+
+def test_short_password(client):
+    resp = post_json(client, '/v1/auth/signup', {
+        "email": "test@test.com",
+        "name": "test",
+        "password": "test"})
+    assert b'Password too short' in resp.data
+    assert resp.status_code == 400
+
+
+def test_long_password(client):
+    resp = post_json(client, '/v1/auth/signup', {
+        "email": "test@test.com",
+        "name": "test",
+        "password": "testsfsfdsfsdf"})
+    assert b'Password too long' in resp.data
+    assert resp.status_code == 400
+
+
+def test_invalid_email(client):
+    resp = post_json(client, '/v1/auth/signup', {
+        "email": "testtest",
+        "name": "test",
+        "password": "testsfsfdsfsdf"})
+    assert b'Invalid email' in resp.data
+    assert resp.status_code == 400
+
+
+def test_empty_post_email(client):
+    resp = post_json(client, '/v1/auth/signup', {
+        "title": " "})
+    assert resp.status_code == 400

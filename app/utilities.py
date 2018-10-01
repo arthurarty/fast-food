557c4db3705516e56fb59479e086b4c8dcac4de3
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-01 22:49:35 +0300

    refractor utilities.

--- app/utilities.py
+++ app/utilities.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify
-
+from flask_jwt_extended import (create_access_token,
+                                get_jwt_identity, jwt_required)
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.models.user import User
+import re
 
 def test_str_input(text):
     """test input to be string"""
@@ -26,13 +30,15 @@ def test_int_input(number):
         return False
 
 def check_for_email_password(email, password):
+    """method ensures email and password exist"""
     if not password:
         return jsonify({"msg": "password field is empty"})
 
     if not email:
         return jsonify({"msg": "email field is empty"}), 400
 
-def create_jwt_token(db_conn, email, create_access_token):
+def create_jwt_token(db_conn, email):
+    """method creates jwt token for a user that logs in successfull"""
     user_id = db_conn.return_id(email)
     user_role = db_conn.return_role(email)
     user_details = {"user_id" : " ", "user_role" : " "}
@@ -42,3 +48,23 @@ def create_jwt_token(db_conn, email, create_access_token):
     output = {'message': 'Successful login'}
     access_token_output = {'access_token': "%s" % (access_token)}
     return jsonify(output, access_token_output), 200
+
+def signup_user(email, name, password, role):
+     if email and name and password and role:
+        if not re.match(r'^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+            return jsonify({"msg": "Invalid email. Example: john@exam.com"}), 400
+
+        if len(name) > 15:
+            return jsonify({"msg": "Name is too long, max 15"}), 400
+
+        if not re.match(r'^[a-z0-9_]+$', name):
+            return jsonify({"msg": "Name can only contain lowercase a-z, 0-9 and _"}), 400
+
+        if len(password) < 8:
+            return jsonify({"msg": "Password too short, min 8 chars"}), 400
+
+        if len(password) > 12:
+            return jsonify({"msg": "Password too long, max 12"}), 400
+
+        new_user = User(email, name, generate_password_hash(password), role)
+        return new_user.insert_new_record()
--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -1,13 +1,8 @@
-from app.models.user import User
-import re
-
 from flask import Flask, jsonify, request
-from flask_jwt_extended import (JWTManager, create_access_token,
-                                get_jwt_identity, jwt_required)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import app
-from app.utilities import *
+from app.utilities import test_str_input, create_jwt_token, check_for_email_password, signup_user
 from app.views import db_conn
 
 
@@ -29,23 +24,7 @@ def add_user():
         return jsonify({"msg":"Role field is empty"}), 400
     
     if email and name and password and role:
-        if not re.match(r'^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
-            return jsonify({"msg": "Invalid email. Example: john@exam.com"}), 400
-
-        if len(name) > 15:
-            return jsonify({"msg": "Name is too long, max 15"}), 400
-
-        if not re.match(r'^[a-z0-9_]+$', name):
-            return jsonify({"msg": "Name can only contain lowercase a-z, 0-9 and _"}), 400
-
-        if len(password) < 8:
-            return jsonify({"msg": "Password too short, min 8 chars"}), 400
-
-        if len(password) > 12:
-            return jsonify({"msg": "Password too long, max 12"}), 400
-
-        new_user = User(email, name, generate_password_hash(password), role)
-        return new_user.insert_new_record()
+        return signup_user(email, name, password, role)
 
     return jsonify({"msg": "empty field"}), 400
 
@@ -66,5 +45,5 @@ def login():
 
     if email in output:
         if check_password_hash(hashed_password[0], password):
-            return create_jwt_token(db_conn, email, create_access_token)
+            return create_jwt_token(db_conn, email)
     return jsonify({"msg":"Bad username or password"}), 400

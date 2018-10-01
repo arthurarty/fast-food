eb2cf77712b4aa5a60fc82455111ab57507d809b
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-01 14:26:59 +0300

    [chore] Add config.py file

---
+++ config.py
@@ -0,0 +1,24 @@
+"""
+module config
+"""
+class Config:
+    """
+    parent config class
+    """
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    """
+    class for development configuration
+    """
+    DEBUG = True
+    DATABASE_URL = 'postgresql://localhost/fastfood'
+
+class TestingConfig(Config):
+    """
+    class for testing configuration
+    """
+    DEBUG = True
+    TESTING = True
+    DATABASE_URL = 'postgresql://localhost/fastfood'
+    

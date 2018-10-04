c87222957a13d78650da2094558a001adfbd5f2a
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-04 18:26:10 +0300

    [chore] Add database url for heroku.

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 """
 module config
 """
+import os
+
 class Config:
     """
     parent config class
@@ -12,7 +14,7 @@ class DevelopmentConfig(Config):
     class for development configuration
     """
     DEBUG = True
-    DATABASE_URL = 'postgres://postgres:asP2Me@localhost:5432/fastfood'
+    DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgres://postgres:asP2Me@localhost:5432/fastfood'
 class TestingConfig(Config):
     """
     class for testing configuration

4f95cf97034ea03e79b9112f89ff9adadda3496d
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-14 19:19:23 +0300

    [ch] Allow cors

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,10 @@ from flask import Flask
 from flasgger import Swagger
 from flask_jwt_extended import JWTManager
 from config import DevelopmentConfig
+from flask_cors import CORS
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(DevelopmentConfig)
 swag = Swagger(app)
 JWTManager(app)
+CORS(app)

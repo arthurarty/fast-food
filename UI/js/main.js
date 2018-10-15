c0d311ac5836f3ddc9f6b44406422ecb1d4aa0e9
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-15 23:45:59 +0300

    [ch] Remove event listner.

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,26 +1,26 @@
-document.getElementById('signin').addEventListener('submit', signin);
-const appUrl = 'http://127.0.0.1:5000/v1/';
-let jwt = '';
+const appUrl = 'http://127.0.0.1:5000/v1/'
+let jwt = ''
 
 /*
 function returns success message 
 */
 function outputSuccess(response) {
-    output = 'Login successful. <br> <p class="center_text">You can now <a href="index.html">Login.</a></p>';
-    //document.getElementById('divSignup').innerHTML = output;
+    output = 'Login successful. <br> <p class="center_text">You can now <a href="view_menu.html">View menu.</a></p>'
+    document.getElementById('divSignup').innerHTML = output;
     jwt = response[1]['access_token']
     console.log(jwt)
+    setTimeout(window.location.replace("view_menu.html"), 9000)
 }
 
 /*
 post email and password
 */
-function signin(event) {
+function signin() {
     event.preventDefault();
     console.log("signin method called")
 
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
 
     fetch(appUrl + 'auth/login', {
         method: 'POST',

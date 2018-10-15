602f4f2c8d5b263cb022bede8f29d0ec4c860792
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-15 23:08:57 +0300

    [ft] Login page output jwt

---
+++ UI/js/main.js
@@ -0,0 +1,34 @@
+document.getElementById('signin').addEventListener('submit', signin);
+const appUrl = 'http://127.0.0.1:5000/v1/';
+let jwt = '';
+
+/*
+function returns success message 
+*/
+function outputSuccess(response) {
+    output = 'Login successful. <br> <p class="center_text">You can now <a href="index.html">Login.</a></p>';
+    //document.getElementById('divSignup').innerHTML = output;
+    jwt = response[1]['access_token']
+    console.log(jwt)
+}
+
+/*
+post email and password
+*/
+function signin(event) {
+    event.preventDefault();
+    console.log("signin method called")
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    fetch(appUrl + 'auth/login', {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({email: email, password: password })
+    }).then((res) => res.json())
+        .then((response) => outputSuccess(response))
+        .catch((err) => console.log(err))
+}
+

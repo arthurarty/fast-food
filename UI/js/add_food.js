8e9e99954f5a288db469487ce8e54a715783440b
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-18 22:20:34 +0300

    [ch] Add success message after adding food menu item

--- UI/js/add_food.js
+++ UI/js/add_food.js
@@ -3,6 +3,19 @@ const appUrl = 'http://127.0.0.1:5000/v1/'
 let jwt = sessionStorage.getItem('jwt')
 
 /*
+function returns success message 
+*/
+function outputSuccess(response) {
+        output = ` <p class="info-success">
+        <span>Message: </span> <br>
+        ${response['msg']} <br>
+        Refresh page to add another item.
+        </p>`
+        document.getElementById('food_menu').innerHTML = output;
+        console.log(response)
+}
+
+/*
 method to add food item to menu
 */
 function add_food(event){
@@ -24,6 +37,6 @@ function add_food(event){
          },
         body: JSON.stringify({food_name: food_name, price: price, desc:desc })
     }).then((res) => res.json())
-        .then((response) => console.log(response))
+        .then((response) => outputSuccess(response))
         .catch((err) => console.log(err)) 
 }

d24725ed6db604a12029285666fac84839825f23
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-19 19:55:15 +0300

    [ft] feature done.

--- UI/js/get_menu.js
+++ UI/js/get_menu.js
@@ -7,9 +7,24 @@ function returns success message
 function outputSuccess(response) {
         let output = ''
         for(let x in response){ 
-                console.log(response[x][0].description)
+                console.log(response[x][0])
+                output = output + `<div class="food_item">
+                <div class="row">
+                        <div class="col-2">
+                            <h3>${response[x][0].name}</h3>
+                        </div>
+                        <div class="col-2">
+                            <h3>Pricing : ${response[x][0].price}/=</h3>
+                        </div>
+                    </div>
+                <p>${response[x][0].description}</p>
+                <form class="quantity_form" action="confirm_order.html" method="get">
+                    <input type="number" name="quantity" placeholder="0" class="form_input" min="1"> 
+                    <input type="submit" class="blue-btn" value="Order">
+                </form>
+            </div>`
             }
-        //document.getElementById('food_menu').innerHTML = output;
+        document.getElementById('food_menu').innerHTML = output;
         //console.log(response)
 
 }

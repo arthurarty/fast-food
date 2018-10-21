f3096a9d76de35b6d38442ae8a6bcc7c4d209dea
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-22 00:12:57 +0300

    [ft] Ui successfully posting order.

--- UI/js/get_menu.js
+++ UI/js/get_menu.js
@@ -18,15 +18,16 @@ function outputSuccess(response) {
                         </div>
                     </div>
                 <p>${response[x][0].description}</p>
-                <form class="quantity_form">
+                <form class="quantity_form" id='post_order'>
                     <input type="number" id="quantity" placeholder="0" class="form_input" min="1"> 
+                    <input type="number" class="form_input_hide" id="menu_id" value="${response[x][0].menu_id}">
                     <input type="submit" class="blue-btn" value="Order">
                 </form>
             </div>`
             }
         document.getElementById('food_menu').innerHTML = output;
         //console.log(response)
-
+        document.getElementById('post_order').addEventListener('submit', post_order);
 }
 
 /*
@@ -46,3 +47,26 @@ function get_menu(){
         .then((response) => outputSuccess(response))
         .catch((err) => console.log(err)) 
 }
+
+/*
+method to add food item to menu
+*/
+function post_order(event){
+        event.preventDefault()
+        console.log("Post order method called")
+
+        let menu_id = Number(document.getElementById('menu_id').value)
+        let quantity = Number(document.getElementById('quantity').value)
+
+        fetch(appUrl + 'users/orders', {
+            method: 'POST',
+            mode: 'cors',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization':`Bearer ${jwt}`
+             },
+            body: JSON.stringify({menu_id: menu_id, quantity: quantity })
+        }).then((res) => res.json())
+            .then((response) => console.log(response))
+            .catch((err) => console.log(err)) 
+    }

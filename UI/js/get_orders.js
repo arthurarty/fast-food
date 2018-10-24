64601ac10e052bc0fc8548bf41be51fe42a52928
ARTY-PC\Arty <arthurnangai@live.com>
2018-10-24 09:30:13 +0300

    [ft] Get single order complete.

--- UI/js/get_orders.js
+++ UI/js/get_orders.js
@@ -15,7 +15,9 @@ function outputSuccess(response) {
                     | User_id: <i>${response[x][0].user_id}</i>
                     | created_at : <i>${response[x][0].created_at}</i> <br>
                     <div class="btn-group">
-                        <a href="" class="green-btn">View order</a>
+                        <button class="green-btn" onclick="get_single_order(${response[x][0].order_id})">
+                        View order
+                        </button>
                     </div>    
                 </div>`
         }
@@ -26,7 +28,7 @@ function outputSuccess(response) {
 method to get orders
 */
 function get_orders(){
-    console.log("Add food method called")
+    console.log("Get order method called")
     console.log(jwt)
     fetch(appUrl + 'orders', {
         method: 'GET',
@@ -37,5 +39,44 @@ function get_orders(){
          }
 }).then((res) => res.json())
         .then((response) => outputSuccess(response))
+        .catch((err) => console.log(err)) 
+}
+
+/*
+out put a single order
+*/
+function output_single_order(response){
+    console.log(response[0])
+    let output = ` <div class="order">
+                    <strong>Order No: <i>${response[0].order_id}</i></strong><br>
+                    Menu_id : <i>${response[0].menu_id}</i><br>
+                    Quantity : <i>${response[0].quantity}</i> <br>
+                    Status: <i>${response[0].status}</i> <br>
+                    User_id: <i>${response[0].user_id}</i><br>
+                    Created_at : <i>${response[0].created_at}</i> <br>
+                    <div class="btn-group">
+                    <button class="green-btn" onclick="get_orders()">
+                    View all orders
+                    </button>
+                </div>  
+                </div>`
+    document.getElementById('orders').innerHTML = output
+}
+
+/*
+method returns a single order
+*/
+function get_single_order(order_id){
+    console.log("Get single order method called")
+    console.log(jwt)
+    fetch(appUrl + `orders/${order_id}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization':`Bearer ${jwt}`
+         }
+}).then((res) => res.json())
+        .then((response) => output_single_order(response))
         .catch((err) => console.log(err)) 
 }
